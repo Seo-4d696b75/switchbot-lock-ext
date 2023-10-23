f4d3f1020be909664fc975a8bf286e2984b703d8
k_senda <k_senda@yumemi.co.jp>
2023-10-23 20:11:29 +0900

    fix: deserialize sealed interface

---
+++ domain/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/domain/device/PhysicalDevice.kt
@@ -0,0 +1,90 @@
+package com.seo4d696b75.android.switchbot_lock_ext.domain.device
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.element
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
+
+@Serializable(with = PhysicalDeviceSerializer::class)
+sealed interface PhysicalDevice {
+    val id: String
+    val name: String
+    val type: String
+    val enableCloudService: Boolean
+    val hubDeviceId: String
+
+    @Serializable
+    data class Base(
+        @SerialName("deviceId")
+        override val id: String,
+        @SerialName("deviceName")
+        override val name: String,
+        @SerialName("deviceType")
+        override val type: String,
+        override val enableCloudService: Boolean,
+        override val hubDeviceId: String,
+    ) : PhysicalDevice
+
+    @Serializable
+    data class Lock(
+        @SerialName("deviceId")
+        override val id: String,
+        @SerialName("deviceName")
+        override val name: String,
+        @SerialName("deviceType")
+        override val type: String,
+        override val enableCloudService: Boolean,
+        override val hubDeviceId: String,
+        val group: Boolean,
+        val groupName: String?,
+        val master: Boolean,
+    ) : PhysicalDevice
+}
+
+private class PhysicalDeviceSerializer : KSerializer<PhysicalDevice> {
+    override val descriptor: SerialDescriptor
+        get() = buildClassSerialDescriptor("PhysicalDevice") {
+            element<String>("deviceType")
+        }
+
+    override fun deserialize(decoder: Decoder): PhysicalDevice {
+        require(decoder is JsonDecoder)
+        val element = decoder.decodeJsonElement()
+        require(element is JsonObject)
+        val type = requireNotNull(element["deviceType"]).jsonPrimitive.content
+        return if (type == "Smart Lock") {
+            decoder.json.decodeFromJsonElement(
+                PhysicalDevice.Lock.serializer(),
+                element,
+            )
+        } else {
+            decoder.json.decodeFromJsonElement(
+                PhysicalDevice.Base.serializer(),
+                element,
+            )
+        }
+    }
+
+    override fun serialize(encoder: Encoder, value: PhysicalDevice) {
+        when (value) {
+            is PhysicalDevice.Base -> encoder.encodeSerializableValue(
+                PhysicalDevice.Base.serializer(),
+                value,
+            )
+
+            is PhysicalDevice.Lock -> encoder.encodeSerializableValue(
+                PhysicalDevice.Lock.serializer(),
+                value,
+            )
+        }
+    }
+
+}
+
--- domain/src/test/java/com/seo4d696b75/android/switchbot_lock_ext/domain/JsonSerializationTest.kt
+++ domain/src/test/java/com/seo4d696b75/android/switchbot_lock_ext/domain/JsonSerializationTest.kt
@@ -1,6 +1,6 @@
 package com.seo4d696b75.android.switchbot_lock_ext.domain
 
-import com.seo4d696b75.android.switchbot_lock_ext.domain.device.LockDevice
+import com.seo4d696b75.android.switchbot_lock_ext.domain.device.PhysicalDevice
 import kotlinx.serialization.json.Json
 import org.junit.Test
 
@@ -12,7 +12,7 @@ class JsonSerializationTest {
         ignoreUnknownKeys = true
     }
     @Test
-    fun serialize_LockDevice() {
+    fun deserialize_LockDevice() {
         val str = """
         {
             "deviceId": "500291B269BE",
@@ -29,8 +29,38 @@ class JsonSerializationTest {
             ]
         }
         """
-        val device: LockDevice = json.decodeFromString(str)
+        val device: PhysicalDevice = json.decodeFromString(str)
         assertEquals("500291B269BE", device.id)
         assertEquals("Door Lock", device.name)
+    }
+
+    @Test
+    fun deserialize_list_PhysicalDevice() {
+
+        val str = """[
+        {
+            "deviceId": "500291B269BE",
+            "deviceName": "Living Room Humidifier",
+            "deviceType": "Humidifier",
+            "enableCloudService": true,
+            "hubDeviceId": "000000000000"
+        },
+        {
+            "deviceId": "500291B269BE",
+            "deviceName": "Door Lock",
+            "deviceType": "Smart Lock",
+            "enableCloudService": true,
+            "hubDeviceId": "000000000000",
+            "group": true,
+            "groupName": "Door",
+            "master": true,
+            "lockDevicesIds": [
+            "000000000001",
+            "000000000002"
+            ]
+        }
+        ]"""
+        val devices: List<PhysicalDevice> = json.decodeFromString(str)
+        assertEquals("500291B269BE", devices[1].id)
     }
 }
