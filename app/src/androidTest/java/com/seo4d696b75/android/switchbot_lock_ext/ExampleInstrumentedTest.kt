82b897089c8eae9d0b9693042c259ff004ac172f
k_senda <k_senda@yumemi.co.jp>
2023-10-23 15:51:23 +0900

    initial commit

---
+++ app/src/androidTest/java/com/seo4d696b75/android/switchbot_lock_ext/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.seo4d696b75.android.switchbot_lock_ext
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.seo4d696b75.android.switchbot_lock_ext", appContext.packageName)
+    }
+}
