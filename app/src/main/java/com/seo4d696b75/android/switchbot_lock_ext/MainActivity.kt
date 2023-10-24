e927bc3d2c0b86ecb445027fe5ffe657a234043d
k_senda <k_senda@yumemi.co.jp>
2023-10-24 19:41:28 +0900

    fix: MainActivity

--- app/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/MainActivity.kt
+++ app/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.seo4d696b75.android.switchbot_lock_ext
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,9 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.seo4d696b75.android.switchbot_lock_ext.domain.device.DeviceRepository
 import com.seo4d696b75.android.switchbot_lock_ext.ui.theme.AppTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var repository: DeviceRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -24,6 +37,15 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                 }
+            }
+        }
+
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                val status = repository.getLockDeviceStatus("E8893CF06602")
+                Log.d("Api", "$status")
+                val res = repository.getLockDevices()
+                Log.d("Api", "$res")
             }
         }
     }
