d54a41513cc30be755818ca6425bdbd05eca456d
Seo-4d696b75 <s.kaoru509@gmail.com>
2023-11-18 19:08:34 +0900

    rm: LockWidgetRepository.kt

--- app/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/widget/AppWidgetReceiver.kt
+++ app/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/widget/AppWidgetReceiver.kt
@@ -3,20 +3,17 @@ package com.seo4d696b75.android.switchbot_lock_ext.widget
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import com.seo4d696b75.android.switchbot_lock_ext.domain.widget.AppWidgetMediator
-import com.seo4d696b75.android.switchbot_lock_ext.domain.widget.LockWidgetRepository
 import com.seo4d696b75.android.switchbot_lock_ext.ui.widget.LockWidget
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class AppWidgetReceiver : GlanceAppWidgetReceiver() {
-    @Inject
-    lateinit var widgetRepository: LockWidgetRepository
 
     @Inject
     lateinit var widgetMediator: AppWidgetMediator
 
     override val glanceAppWidget: GlanceAppWidget by lazy {
-        LockWidget(widgetRepository, widgetMediator)
+        LockWidget(widgetMediator)
     }
 }
--- domain/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/domain/widget/LockWidgetRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.seo4d696b75.android.switchbot_lock_ext.domain.widget
-
-import kotlinx.coroutines.flow.Flow
-
-interface LockWidgetRepository {
-    val stateProviderFlow: Flow<LockWidgetStateProvider>
-    fun setState(deviceId: String, state: LockWidgetState)
-}
