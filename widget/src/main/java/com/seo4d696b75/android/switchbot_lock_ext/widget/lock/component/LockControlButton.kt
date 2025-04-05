97a8090a3706bef4b16fe93a942d6db6028f1c86
Seo-4d696b75 <s.kaoru509@gmail.com>
2025-04-05 20:41:10 +0900

    feat: update lock widget ui

--- widget/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/widget/lock/component/LockControlButton.kt
+++ widget/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/widget/lock/component/LockControlButton.kt
@@ -2,7 +2,6 @@ package com.seo4d696b75.android.switchbot_lock_ext.widget.lock.component
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.glance.ColorFilter
 import androidx.glance.ExperimentalGlanceApi
 import androidx.glance.GlanceModifier
@@ -12,12 +11,9 @@ import androidx.glance.ImageProvider
 import androidx.glance.action.clickable
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
-import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import androidx.glance.layout.size
-import androidx.glance.text.Text
-import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
 import com.seo4d696b75.android.switchbot_lock_ext.theme.R
 
@@ -42,26 +38,13 @@ fun LockControlButton(
             colorFilter = ColorFilter.tint(color),
             modifier = GlanceModifier.fillMaxSize(),
         )
-        Column(
+        Image(
+            provider = icon,
+            contentDescription = label,
+            colorFilter = ColorFilter.tint(GlanceTheme.colors.onPrimary),
             modifier = GlanceModifier
                 .fillMaxSize()
                 .padding(2.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            Image(
-                provider = icon,
-                contentDescription = label,
-                colorFilter = ColorFilter.tint(GlanceTheme.colors.onPrimary),
-                modifier = GlanceModifier.size(30.dp),
-            )
-            Text(
-                text = label,
-                style = TextStyle(
-                    color = GlanceTheme.colors.onPrimary,
-                    fontSize = 10.sp,
-                ),
-            )
-        }
+        )
     }
 }
