3a25bb5f809cad43441bceeee47ded9a2961ada1
Seo-4d696b75 <s.kaoru509@gmail.com>
2025-04-20 18:53:54 +0900

    docs: inapp widget problem

--- app/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/widget/AppWidgetMediatorImpl.kt
+++ app/src/main/java/com/seo4d696b75/android/switchbot_lock_ext/widget/AppWidgetMediatorImpl.kt
@@ -1,10 +1,13 @@
 package com.seo4d696b75.android.switchbot_lock_ext.widget
 
+import android.app.ActivityOptions
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.core.os.bundleOf
 import androidx.glance.appwidget.GlanceAppWidgetManager
 import com.seo4d696b75.android.switchbot_lock_ext.domain.widget.AppWidgetConfiguration
 import com.seo4d696b75.android.switchbot_lock_ext.domain.widget.AppWidgetMediator
@@ -85,11 +88,28 @@ class AppWidgetMediatorImpl @Inject constructor(
                 AppWidgetType.Standard -> LockWidgetConfigureActivity.InApp::class
                 AppWidgetType.Simple -> SimpleLockWidgetConfigureActivity.InApp::class
             }
+            // FIXME ウィジェットをドラッグで位置調整しながら追加すると設定Activityが起動しない
+            // アプリがバックグランドになってAndroid14以降の制約に抵触する？
+            // https://developer.android.com/guide/components/activities/background-starts?hl=ja#opt-in-required
+            // PendingIntent.send はアプリ外から呼ばれるので制御できない
+            val options =
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                    ActivityOptions.makeBasic()
+                        .setPendingIntentCreatorBackgroundActivityStartMode(
+                            ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED
+                        )
+                        .toBundle()
+                } else {
+                    bundleOf(
+                        "android.activity.pendingIntentCreatorBackgroundActivityStartMode" to 1
+                    )
+                }
             val callback = PendingIntent.getActivity(
                 context,
                 deviceId.hashCode(),
                 Intent(context, configActivity.java),
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE,
+                options,
             )
             val result = manager.requestPinAppWidget(provider, null, callback)
             if (result) {
